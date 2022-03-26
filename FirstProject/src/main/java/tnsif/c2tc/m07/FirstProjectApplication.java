afc34e197645eb39d0903fb7237d4dcb9ee5fce7
pc <pc@DESKTOP-GO91MJH>
2022-03-25 17:09:09 -0700

    my first spring project

--- FirstProject/src/main/java/tnsif/c2tc/m07/FirstProjectApplication.java
+++ FirstProject/src/main/java/tnsif/c2tc/m07/FirstProjectApplication.java
@@ -3,12 +3,15 @@ package tnsif.c2tc.m07;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tnsif.c2tc.m07.entity.Student;
+
 @SpringBootApplication
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectApplication.class, args);
-		System.out.println("Hello World");
+		Student student=new Student();
+		student.display();
 	}
 
 }
---
+++ FirstProject/src/main/java/tnsif/c2tc/m07/entity/Student.java
@@ -0,0 +1,22 @@
+package tnsif.c2tc.m07.entity;
+
+public class Student {
+	private int uid;
+	private String name;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void display()
+	{
+		System.out.println("In Student class");
+	}
+}
